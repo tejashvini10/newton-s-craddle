c4275868b9ebc35053f253334f71798b4c062333
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-01-21 18:34:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,11 +28,11 @@ function setup() {
 	
 	startBobPositionX=width/2;
 	startBobPositionY=height/4+500;
-	bobObject1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
-	bobObject2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
-	bobObject3=new Bob(startBobPositionX,startBobPositionY,bobDiameter);
-	bobObject4=new Bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
-	bobObject5=new Bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
+	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
 	
 	
 	var render=Render.create({
